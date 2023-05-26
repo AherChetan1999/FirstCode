30d8e8923e0a0f17a7fc579ee89899a7a2565143
Aher Chetan <69471574+AherChetan1999@users.noreply.github.com>
2023-05-26 13:26:19 +0530

    this is the Task no. 4

---
+++ NewTask/demo.js
@@ -0,0 +1,6 @@
+var items=document.getElementsByClassName('list-group-item');
+//console.log(items[4]);
+items[2].style.backgroundColor='green';
+
+var header=document.getElementById("items");
+header.style.fontWeight ='bold';
