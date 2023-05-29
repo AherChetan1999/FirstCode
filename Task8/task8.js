ec1ab95e57d0131a1c03f86aa7762d1486bbc875
Aher Chetan <69471574+AherChetan1999@users.noreply.github.com>
2023-05-29 12:13:04 +0530

    Task 9

--- Task8/task8.js
+++ Task8/task8.js
@@ -15,13 +15,15 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var newItem2 = document.getElementById('item2').value;
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newItem ));
+  li.appendChild(document.createTextNode( newItem2));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
@@ -72,7 +74,9 @@ function filterItems(e){
   // Convert to an array
   Array.from(items).forEach(function(item){
     var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
+    var description = item.childNodes[1].textContent;
+    
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
       item.style.display = 'block';
     } else {
       item.style.display = 'none';
